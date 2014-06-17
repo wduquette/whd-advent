939a0752b38dfeb0efd1b24c9a76a298dc0edc10
wduquette <will@wjduquette.com>
2014-06-16 17:41:07 -0700

    Added (define-room)
    Rooms are now defined using the (define-room) function, and stored in
    the `rooms` atom.  The room descriptions are wrapped to 72 columns
    automatically.

--- src/whd_advent/rooms.clj
+++ src/whd_advent/rooms.clj
@@ -3,18 +3,21 @@
 ;;;; Room definitions for Will's Text Adventure
 
 (ns whd-advent.rooms
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:use whd-advent.tools))
 
 ;;; ## Directions
 ;;;
 ;;; Directions are stored internally as keywords.  The values
-;;; in the dir-names map are for display, rather than for input.
+;;; in the dir-names map are for display, rather than for input; see 
+;;; `whd-advent.vocab` for the command vocabulary. 
 
 (def dir-names {
   :n "North",
   :s "South",
   :e "East",
   :w "West"})
+
 
 ;;; ## Defining Rooms
 ;;;
@@ -26,44 +29,47 @@
 ;;;    keywords
 ;;;  * `:description` - The room's full description.
 ;;;  * Any arbitrary keywords and values needed as the game is developed.
+;;;
+;;; Taken altogether, the rooms form the world map, which is stored in an atom 
+;;; called `rooms`, and consists of a map from room keywords to room structures.
+;;; Use `(define-room ...)` to add a room to the map.
+
+(def rooms (atom {}))
 
 (defn- make-room
   "Returns a room structure, given the room's title, links, and description,
   and any optional data values.  "
   [title links description & extra-info]
-  (merge {:title title :links links :description description}
+  (merge {:title title :links links :description (wrap-text description)}
          (apply hash-map extra-info)))
 
+(defn define-room 
+  "Creates a room structure, given the room's ID, title, links, and description,
+  and any optional data values, and adds it to the world map."
+  [kw & room-info]
+  (swap! rooms assoc kw (apply make-room room-info)))
 
-;;; ## The Game Map
-;;;
-;;; The game map is stored as a map from room keywords to room structures
-;;; in the "rooms" variable.  There are a couple of other things I could do.
-;;;
-;;; 1. The rooms var could contain an atom; then, a (define-room) function
-;;;    could define a complete room and associate it with the atom.  This
-;;;    would be appropriate if I were implementing an adventure game
-;;;    framework into which the game data was loaded.
-;;;
-;;; 2. Rooms could be defined in separate modules (i.e., "areas"), and then
-;;;    all rooms could be pulled together by passing the area vars to one
-;;;    function that initialized the rooms var.  This function could possibly
-;;;    do some computation or sanity checking or caching at the same time,
-;;;    all without using mutable variables.
+  
 
-(def rooms {
-  :home
-    (make-room "Home Base" {:n :street}
-               "Your home base is the picture of comfort.")
-  :street
-    (make-room "The Street" {:s :home :e :corner :w :park}
-               "It's gritty here in the mean streets.")
-  :corner
-    (make-room "The Corner" {:w :street}
-               "You're down on the corner.")
-  :park
-    (make-room "Neighborhood Park" {:e :street}
-               "You're in the park.")})
+
+;;; ## World Map
+;;;
+;;; This is the world map data
+(define-room :home "Home Base" 
+  {:n :street}
+  "Your home base is the picture of comfort.")
+
+(define-room :street "The Street" 
+  {:s :home :e :corner :w :park}
+  "It's gritty here in the mean streets.")
+
+(define-room :corner "The Corner"
+  {:w :street}
+  "You're down on the corner.")
+
+(define-room :park "Neighborhood Park"
+  {:e :street}
+  "You're in the park.")
 
 ;;; ## Room Queries
 ;;;
@@ -73,7 +79,7 @@
   "Returns true if the given value is a room keyword, and false
    otherwise."
   [room]
-  (if (rooms room) true false))
+  (if (@rooms room) true false))
 
 (defn is-dir?
   "Returns true if the given value is a direction keyword, and false
@@ -86,18 +92,18 @@
   nil if none.  Ultimately we can put in logic to allow computed 
   destinations or blocked links."
   [room dir]
-  (-> rooms room :links dir))
+  (-> @rooms room :links dir))
 
 (defn room-title 
   "Returns the room's title."
   [room]
-  (-> rooms room :title))
+  (-> @rooms room :title))
 
 (defn describe-room
   "Returns a text description of a room, including the directions you can
   go.  Ultimately, this should probably take some options, indicating which
   information you want to include or not include in the return value."
   [room]
-  (let [{:keys [title links description]} (rooms room)]
+  (let [{:keys [title links description]} (@rooms room)]
     (format "%s\n%s\n\nYou can go: %s\n" title description
             (str/join ", " (map dir-names (keys links))))))
